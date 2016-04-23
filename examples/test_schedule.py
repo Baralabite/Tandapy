e81549fbe739a1e12e71f59ffbe0c5c0a878dffd
John Board <johnrobboard@gmail.com>
2016-04-23 14:36:25 +1000

    Added rostering

---
+++ examples/test_schedule.py
@@ -0,0 +1,35 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2016 John Board
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from Tandapy.tanda import Tanda
+from Tandapy.credentials import TOKEN
+
+tanda = Tanda()
+tanda.authenticate(TOKEN)
+
+departments = tanda.getSchedule()
+departmentIDs = departments.getIDs()
+department = departments.getEntry(departmentIDs[0])
+
+print(dir(department))
